04c79e0c9b5a14c165bc4c7ed289dc9c74c049a0
atichat lappanopakron <boss_atln@outlook.com>
2018-11-11 20:59:49 +0700

    full test 11/11 20:59

--- test_case_assign_cashier_job.py
+++ test_case_assign_cashier_job.py
@@ -14,62 +14,63 @@ desired_cap = {
 }
 username = os.environ['SAUCE_USERNAME']
 access_key = os.environ['SAUCE_ACCESS_KEY']
-driver = webdriver.Remote(
+selenium = webdriver.Remote(
    command_executor='https://{}:{}@ondemand.saucelabs.com/wd/hub'.format(username, access_key),
    desired_capabilities=desired_cap)
  
-# This is your test logic. You can add multiple tests here.
-driver.get("http://www.google.com")
+# Test driver
+selenium.get("http://www.google.com")
  
-if "Google" not in driver.title:
+if "Google" not in selenium.title:
     raise Exception("Unable to load google page!")
-elem = driver.find_element_by_name("q")
+
+elem = selenium.find_element_by_name("q")
 elem.send_keys("Sauce Labs")
 elem.send_keys(Keys.TAB)
 elem.submit()
-assert "Sauce" in driver.title
 
+assert "Sauce" in selenium.title
 
 # # Redirect to login page
-# selenium.get('http://127.0.0.1:8000/srd_web/signin')
-# time.sleep(1)
-# assert 'srd_web/signin' in selenium.current_url
+selenium.get('http://127.0.0.1:8000/srd_web/signin')
+time.sleep(1)
+assert 'srd_web/signin' in selenium.current_url
 
 # # Find Elements to Login
-# username = selenium.find_element_by_id('username')
-# password = selenium.find_element_by_id('password')
-# submit = selenium.find_element_by_id('submit') 
+username = selenium.find_element_by_id('username')
+password = selenium.find_element_by_id('password')
+submit = selenium.find_element_by_id('submit') 
 
 # # Fill them to fields
-# username.send_keys('hello')
-# password.send_keys('123456')
-# submit.click()
+username.send_keys('hello')
+password.send_keys('123456')
+submit.click()
 
 # # Redirect to monitor page
-# selenium.get('http://127.0.0.1:8000/srd_web/index/2018-11-02')
-# time.sleep(1)
-# assert 'srd_web/index' in selenium.current_url
+selenium.get('http://127.0.0.1:8000/srd_web/index/2018-11-02')
+time.sleep(1)
+assert 'srd_web/index' in selenium.current_url
 
 # # Find nav-assign elements to redirect to assign page
-# nav_assign = selenium.find_element_by_id('nav-assign')
-# nav_assign.click()
-# time.sleep(1)
-# assert 'srd_web/assign' in selenium.current_url 
+nav_assign = selenium.find_element_by_id('nav-assign')
+nav_assign.click()
+time.sleep(1)
+assert 'srd_web/assign' in selenium.current_url 
 
 # # Select cashier in dropdown menu
-# dropdown = Select(selenium.find_element_by_class_name('f-1'))
-# dropdown.select_by_visible_text("pop pongkul")
-# submit = selenium.find_element_by_id('submit')
-# submit.click()
+dropdown = Select(selenium.find_element_by_class_name('f-1'))
+dropdown.select_by_visible_text("pop pongkul")
+submit = selenium.find_element_by_id('submit')
+submit.click()
 
 # # Redirect to monitor page
-# nav_assign = selenium.find_element_by_id('nav-index')
-# nav_assign.click()
+nav_assign = selenium.find_element_by_id('nav-index')
+nav_assign.click()
 
 # # Check the record has been changed and same name cahier 
-# cashier_name = selenium.find_element_by_id('cashier3')
-# assert cashier_name.text == "pop pongkul"
+cashier_name = selenium.find_element_by_id('cashier3')
+assert cashier_name.text == "pop pongkul"
 
 # # Close window
-# selenium.close()
-# selenium.quit()
+selenium.close()
+selenium.quit()
