9e5293447cc28a9ff9792528f301671bd545753c
atichat lappanopakron <boss_atln@outlook.com>
2018-11-11 21:11:45 +0700

    test 11/11 21:11

--- test_case_assign_cashier_job.py
+++ test_case_assign_cashier_job.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
   
 desired_cap = {
-    'platform': "Mac OS X",
+    'platform': "Mac OS X 10.13",
     'browserName': "chrome",
     'version': "latest",
 }
