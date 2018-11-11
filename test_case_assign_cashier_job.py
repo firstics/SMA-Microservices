5455d68d5e3ffc92ec4e433ac2943c927163b18e
atichat lappanopakron <boss_atln@outlook.com>
2018-11-11 21:07:02 +0700

    test 11/11 21:06 change os: osx

--- test_case_assign_cashier_job.py
+++ test_case_assign_cashier_job.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
   
 desired_cap = {
-    'platform': "Mac OS X 10.12",
+    'platform': "Mac OS X",
     'browserName': "chrome",
     'version': "latest",
 }
