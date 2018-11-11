bd8cb9708425d2d9d258d2d922fd3529cbe6645a
atichat lappanopakron <boss_atln@outlook.com>
2018-11-11 21:21:48 +0700

    test 11/11 21:21 back to Linux

--- test_case_assign_cashier_job.py
+++ test_case_assign_cashier_job.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
   
 desired_cap = {
-    'platform': "Mac OS X 10.13",
+    'platform': "Linux",
     'browserName': "chrome",
     'version': "latest",
 }
