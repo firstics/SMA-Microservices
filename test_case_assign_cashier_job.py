ad41802a42a2926c8757f313475334fdb2c1c1ab
atichat lappanopakron <boss_atln@outlook.com>
2018-11-11 20:55:14 +0700

    test 11/11 20:55

--- test_case_assign_cashier_job.py
+++ test_case_assign_cashier_job.py
@@ -2,11 +2,32 @@
 import time
 import os
 from os import environ
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
   
-username = environ.get('SAUCE_USERNAME', None)
-access_key = environ.get('SAUCE_ACCESS_KEY', None)
-selenium_endpoint = "http://%s:%s@ondemand.saucelabs.com:80/wd/hub" % (username, access_key)
-print(selenium_endpoint)
+desired_cap = {
+    'platform': "Mac OS X 10.12",
+    'browserName': "chrome",
+    'version': "latest",
+}
+username = os.environ['SAUCE_USERNAME']
+access_key = os.environ['SAUCE_ACCESS_KEY']
+driver = webdriver.Remote(
+   command_executor='https://{}:{}@ondemand.saucelabs.com/wd/hub'.format(username, access_key),
+   desired_capabilities=desired_cap)
+ 
+# This is your test logic. You can add multiple tests here.
+driver.get("http://www.google.com")
+ 
+if "Google" not in driver.title:
+    raise Exception("Unable to load google page!")
+elem = driver.find_element_by_name("q")
+elem.send_keys("Sauce Labs")
+elem.send_keys(Keys.TAB)
+elem.submit()
+assert "Sauce" in driver.title
 
 
 # # Redirect to login page
