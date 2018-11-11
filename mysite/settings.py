b6c0e132c0416f9e4da7c47ad6e1e36bf16c0ad2
Jethnithi <bagpigham@gmail.com>
2018-11-11 23:24:37 +0700

    whitenoise v3

--- mysite/settings.py
+++ mysite/settings.py
@@ -67,7 +67,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
